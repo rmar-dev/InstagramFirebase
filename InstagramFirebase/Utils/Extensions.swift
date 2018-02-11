72a69d0b7cb391e385b612d45b6d08939630d960
Ricardo Rabeto <ricardomarabeto@gmail.com>
2018-02-11 15:56:08 +0000

    New feature: 	extension to place constraints to a view

--- InstagramFirebase/Utils/Extensions.swift
+++ InstagramFirebase/Utils/Extensions.swift
@@ -14,3 +14,36 @@ extension UIColor {
         return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
     }
 }
+
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?, paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, height: CGFloat){
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top{
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
